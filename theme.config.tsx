4afda47cc06029b1b2eaaf936c9cbec499315738
cocdeshijie <2865509822ma@gmail.com>
2023-04-09 01:49:02 -0400

    head

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,14 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 import {useRouter} from "next/router";
 
 const config: DocsThemeConfig = {
+  head: (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:url" content="https://antapi.org" />
+        <meta property="og:title" content="antapi.org" />
+        <meta property="og:description" content="by cocdeshijie" />
+      </>
+  ),
   logo: <span>antapi.org</span>,
   project: {
     link: 'https://github.com/cocdeshijie/antapi.org',
